5f708fc14477218cdd49db60f4eab1deae271e57
Carlos Contreras <carconval@gmail.com>
2025-11-28 22:04:31 -0500

    upgraded appId

--- Sources/v2.0/capacitor.config.ts
+++ Sources/v2.0/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.blindsbook.draperycalculator',
-  appName: 'Drapery-Calculator-Vue',
+  appId: 'com.blindsbook.drap-calc',
+  appName: 'BlindsBook Drapery Calculator',
   webDir: 'dist'
 };
 
